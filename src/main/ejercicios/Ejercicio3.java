5ea2f70a6a6df893d4d8ed245ccb7ec7804ace1f
DavidFidgett <fidgettdavid@gmail.com>
2021-07-12 20:43:44 -0500

    Agregando comentarios.

--- src/main/ejercicios/Ejercicio3.java
+++ src/main/ejercicios/Ejercicio3.java
@@ -4,7 +4,32 @@ import graph.WeightedGraph;
 
 import java.util.Scanner;
 
+/**
+ * Clase correspondiente al Ejercicio #3
+ */
 public class Ejercicio3 {
+    /**
+     * Metodo correspondiente a la Clase Principal del Ejercicio #3
+     * Implementacion de Grafos Ponderados.
+     * A diferencia del {@link Ejercicio2#run()} donde
+     * se tenia que implementar un grafo dirigido, en este caso
+     * se implemento un Grafo Ponderado, haciendo pocas modificaciones
+     * a la clase {@link graph.SimpleGraph}. Estas modificaciones consistieron
+     * en simplemente agregar el parametro de weight a los metodos de
+     * esta clase para que se agregara el costo de la arista a las conexiones
+     * en los nodos que componen la lista de adyacencia.
+     * Debido a que, en similitud con el grafo simple, en esta implementacion
+     * del grafo ponderado es tambien un grafo no dirigido, se mantuvieron
+     * las relaciones de los nodos de manera bilateral tanto para agregar como
+     * para eliminar aristas.
+     * De igual manera, en este caso se realizo un menu para que el usuario
+     * ingrese la cantidad de nodos que quiere que contenga el grafo, para
+     * posteriormente en el menu poder seleccionar las opciones que le permitan
+     * ver la lista de adyacencia del grafo, agregar aristas con un costo, o
+     * eliminar estas mismas.
+     * La ultima opcion del menu es la de terminar la ejecucion de este
+     * metodo.
+     */
     public static void run(){
         Scanner sc = new Scanner(System.in);
         System.out.println("\n Ejercicio 3.- Grafos Ponderados");
